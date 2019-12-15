5752ab5a690b4372be84b0558dae23d9f1aa065c
LENOVO <LENOVO@DESKTOP-3O6JRSI>
2019-12-16 00:03:32 +0200

    this is java project about center courses system.

---
+++ src/coursessystem/CoursesSystem.java
@@ -0,0 +1,15 @@
+
+package coursessystem;
+
+import java.util.Scanner;
+
+
+public class CoursesSystem {
+ public Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+         ui u = new ui();
+         u.main();
+    }
+    
+}
